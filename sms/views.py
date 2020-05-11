0c8b2054e503f003fb0edb910c5625d5b79aac94
Matt <MattisTrees@gmail.com>
2020-05-11 20:45:11 +0000

    Edit views.py to parse GET requests instead of POST requests. Made change on Twilio website so that text messages send as GET requests as POST requests were sent without parameters. We can now determine the body of the text message and branch based on user input. Still need to figure out how to make the Django App Stateful instead of Stateless

--- sms/views.py
+++ sms/views.py
@@ -8,17 +8,25 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 @csrf_exempt
 def sms_response(request):
-	print(str(request))
+	#print(str(request))
 
 	# Start the TwiML response
 	resp = MessagingResponse()
 
 	if request.method == "POST":
-
 		# Add a text message
-		msg = resp.message("Hello Lauren! This is a message from the Django App!!!")
+		msg = resp.message("Hello There! The App has recieved a POST request and doesn't know what to do with it! Ask Matt what to do!")
 
 		return HttpResponse(str(resp))
 	if request.method == "GET":
-		msg = resp.message("The Django server has recieved a GET request and doesn't know what to do with it! Ask Matt!")
+		UserInput = str(request.GET.get('Body'))
+		msg = resp.message(determine_response(UserInput))
+		print(UserInput)
 		return HttpResponse(str(resp))
+
+
+def determine_response(UserInput):
+	if UserInput == "Hello":
+		return "Hello to you, too!"
+	else:
+		return "Please say 'Hello', that's the only word I know."
