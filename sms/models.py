ae732b808932051937b1be571f3332431cbb5d0c
Matt <MattisTrees@gmail.com>
2020-06-20 19:42:28 +0000

    Add Django Models to INSTALLED_APPS in settings.py. This allows me to manipulate models in views.py

--- sms/models.py
+++ sms/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+	phone = models.CharField('Phone Number', max_length=12)
+	previous_response = models.CharField('Last Response', max_length=160)
